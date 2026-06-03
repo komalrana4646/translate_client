51a8805a321718f297b820748a6871a4f17d95a0
komal <komalrana4646@gmail.com>
2026-06-03 16:54:44 +0530

    updated12

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,1 +1,2 @@
-export const local = 'https://translate-server-pghn.onrender.com/'
+// export const local = 'https://translate-server-pghn.onrender.com/'
+export const local = 'http://localhost:2003/'
